697b4c48f7bd742f196d9bbcb4d0c1f21a5737df
DESKTOP-U9B2EMS\15153 <15153234535@139.com>
2025-02-10 16:28:44 +0800

    Clear all files from the branch

--- app/(home)/database/items/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DatabaseTabs } from "@/components/database-tabs/page"
-
-export default function DatabaseItemsPage() {
-  return (
-    <div className="p-6">
-      <DatabaseTabs />
-    </div>
-  )
-}
