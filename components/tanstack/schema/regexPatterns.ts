fb46258c7820c71982fad978c7cc629814c97171
DESKTOP-U9B2EMS\15153 <15153234535@139.com>
2025-03-12 08:32:18 +0800

    Clear all files from the branch

--- components/tanstack/schema/regexPatterns.ts
+++
@@ -1,7 +0,0 @@
-export const telephoneRegex = /^(?:\+?\d{1,3}[-.\s]?)?\d{3}[-.\s]?\d{3}[-.\s]?\d{4}$/;
-export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-export const zipCodeRegex = /^\d{5}(-\d{4})?$/; // 
-
-export const glnRegex = /^\d{13}$/;
-export const ffrnRegex = /^\d{11}$/;
-export const gtinRegex = /^\d{13}$/;
