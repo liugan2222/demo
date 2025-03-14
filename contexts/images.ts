f1a30b8ce7ce9393b08fbe053cf151038876f552
DESKTOP-U9B2EMS\15153 <15153234535@139.com>
2025-03-14 16:08:44 +0800

    Clear all files from the branch

--- contexts/images.ts
+++
@@ -1,7 +0,0 @@
-// src/constants/images.ts
-export const IMAGE_PATHS = {
-    DEFAULT_ITEM: "/default_item_pic.png",
-    DEFAULT_AVATAR: "/default_user_pic.png",
-    DEFAULT_FRESHPOINT_LOGO: "/default_freshpoint_logo.png",
-    DEFAULT_LOGIN_PIC: "/default_login_pic.png",
-  } as const;
